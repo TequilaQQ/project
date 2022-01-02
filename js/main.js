f45f0c1c83f35edf7478bf0729218e622c136261
Evgeniy <nikelodiym1210@gmail.com>
2022-01-02 19:37:07 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// // let dinosaurs = [];
+
+// let dinosaurs = ['T-Rex', 'Velocirapt', 'Stegosaurs',
+//     'Tricerapts', 'Brachiosausr', 'Brachiosausr', [5, 'Brachiosausr'], 'Brachiosausr'
+// ];
+
+// console.log(dinosaurs[6][1]);
+
+
+// let maniacs = ['Yakko', 'Wakko', 'Dot', 'Stegosaurs', 'Stegosaurs', 'Wakko'];
+// maniacs[maniacs.length - 1];
+// maniacs.push('Cat');
+// maniacs.pop();
+// maniacs.unshift('Dog');
+
+// console.log(maniacs[maniacs.length - 1]);
+// console.log(maniacs);
+
+
+// let styles = ['Джаз', 'Блюзз'];
+// styles.push('Рок-н-ролл');
+
+// // console.log(styles);
+// let fruits = ["Яблоко", "Апельсин", "Груша"];
+// alert(fruits.pop());
+// alert(fruits);
+// fruits.push('Груша');
+// alert(fruits);
+
+
+let admin = 'Jonh';
+let name = 'Jonh';
+alert(admin);
