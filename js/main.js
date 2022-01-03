dd663fe7b014303931418e3d4a72476a2490a91a
Evgeniy <nikelodiym1210@gmail.com>
2022-01-03 13:19:09 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,20 @@
-// // let dinosaurs = [];
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// let dinosaurs = ['T-Rex', 'Velocirapt', 'Stegosaurs',
-//     'Tricerapts', 'Brachiosausr', 'Brachiosausr', [5, 'Brachiosausr'], 'Brachiosausr'
-// ];
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
-// console.log(dinosaurs[6][1]);
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-// let maniacs = ['Yakko', 'Wakko', 'Dot', 'Stegosaurs', 'Stegosaurs', 'Wakko'];
-// maniacs[maniacs.length - 1];
-// maniacs.push('Cat');
-// maniacs.pop();
-// maniacs.unshift('Dog');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// console.log(maniacs[maniacs.length - 1]);
-// console.log(maniacs);
-
-
-// let styles = ['Джаз', 'Блюзз'];
-// styles.push('Рок-н-ролл');
-
-// // console.log(styles);
-// let fruits = ["Яблоко", "Апельсин", "Груша"];
-// alert(fruits.pop());
-// alert(fruits);
-// fruits.push('Груша');
-// alert(fruits);
-
-
-let admin = 'Jonh';
-let name = 'Jonh';
-alert(admin);
+console.log(personalMovieDB);
